b1da21364409060689dde3ca8af93a23fa3fd5e6
Dmytro Chepurnyi <dmytro.chepurnyi@gmail.com>
2015-10-05 10:14:31 +0300

    React updated to 0.14.0-rc1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+// react should be included when use ReactDom
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { App } from './App';
 
-React.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
